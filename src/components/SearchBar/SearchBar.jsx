265772e2afcc83701bcfd14f1416c96a697a17fd
salmaa-ibrahim <salmaa.ibrahimm19@gmail.com>
2025-10-09 13:01:11 +0300

    new

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import products from '../../Data/products';
+import products from '../../data/products';
 import './SearchBar.css';
 import SearchIcon from '../../../assets/Icons/search-icon.svg'; // تأكد من مسار الأيقونة
 
