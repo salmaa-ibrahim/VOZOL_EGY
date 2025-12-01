ddee7d7abada6ee842d6b4c971f7c0f00d8ac307
salmaa-ibrahim <salmaa.ibrahimm19@gmail.com>
2025-12-01 22:48:29 +0200

    new

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1103,7 +1103,7 @@ const Checkout = () => {
     try {
       // تنسيق المنتجات بشكل منظم للبريد الإلكتروني
       const formattedItems = orderData.items.map(item => 
-        `• ${item.name} (${item.flavor}) <br> - ${item.quantity}  قطعة × ${item.price} جنيه = ${item.price * item.quantity} جنيه`
+        `• ${item.name} (${item.flavor}) <br> - ${item.quantity}  قطعة × ${item.price} جنيه = ${item.price * item.quantity} جنيه <br>`
       ).join('<br>');
 
       // إعداد بيانات القالب - إرسال للمدير فقط
