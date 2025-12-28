8fab4b3a2c1108ae46f84f0d5d82d8c8d4284d90
salmaa-ibrahim <salmaa.ibrahimm19@gmail.com>
2025-12-28 20:16:04 +0200

    new

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,8 @@
-
 import React from "react";
 // import { useState } from "react";
 import { useCart } from "../../contexts/CartContext";
 import "./ProductCard.css";
-import { useNavigate , Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 export default function ProductCard({ product }) {
   const { addItem } = useCart();
   // const [flavor, setFlavor] = useState(product.flavor || "");
@@ -24,12 +23,24 @@ export default function ProductCard({ product }) {
           <img src={product.img} alt={product.name} />
         </div>
       </Link>
-      <p>{product.flavor} <br /> <span className={product.inStock ? "In-Stock" : "Out-of-Stock"}>{product.inStock ? "In Stock" : "Out of Stock"}</span> 
-                                 <span className={product.sale ? "Sale" : ""}>{product.sale ? "Sale" : ""}</span> 
+      <p>
+        {product.flavor} <br />{" "}
+        <span className={product.inStock ? "In-Stock" : "Out-of-Stock"}>
+          {product.inStock ? "In Stock" : "Out of Stock"}
+        </span>
+        <span className={product.sale ? "Sale" : ""}>
+          {product.sale ? "Sale" : ""}
+        </span>
       </p>
       <div className="meta">{product.name}</div>
       {/* <div className="description">{product.description}</div> */}
-      <div className="price"> <span>EGP {product.price} <span className="old-price">{product.oldPrice}</span></span></div> 
+      <div className="price">
+        {" "}
+        <span>
+          EGP {product.price}{" "}
+          <span className="old-price">{product.oldPrice}</span>
+        </span>
+      </div>
       <div className="actionsBtn">
         <button className="btn" onClick={handleAddToCart}>
           Add to cart
