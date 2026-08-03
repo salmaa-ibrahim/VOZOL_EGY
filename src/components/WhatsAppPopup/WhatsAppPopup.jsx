3838fdd9f0605e078d91a154d0b0d5d712285205
Salma Ibrahim Abd El-Monem <salmaa.ibrahimm19@gmail.com>
2026-08-03 19:34:16 +0300

    Update WhatsAppPopup.jsx

--- src/components/WhatsAppPopup/WhatsAppPopup.jsx
+++ src/components/WhatsAppPopup/WhatsAppPopup.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './WhatsAppPopup.css';
 
 const WhatsAppPopup = ({
-  phoneNumber = "2015562209222",
+  phoneNumber = "201556220922",
   message = "محتار؟ محتاج مساعده؟ كلمنا واتساب ونساعدك فورًا"
 }) => {
   const [isVisible, setIsVisible] = useState(false);
