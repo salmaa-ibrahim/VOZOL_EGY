813f23ca95652f3bbd08cadcb0c56748f1b9ebb5
Salma Ibrahim Abd El-Monem <salmaa.ibrahimm19@gmail.com>
2026-09-02 10:03:21 +0300

    Update WhatsAppFab.jsx

--- src/components/WhatsAppFab/WhatsAppFab.jsx
+++ src/components/WhatsAppFab/WhatsAppFab.jsx
@@ -6,7 +6,7 @@ export default function WhatsAppFab() {
   return (
     <a
       className="wa-fab"
-      href="https://wa.me/201505337422?text=مرحباً، أريد الاستفسار عن المنتجات"
+      href="https://wa.me/201556220922?text=مرحباً، أريد الاستفسار عن المنتجات"
       target="_blank"
       rel="noreferrer"
     >
